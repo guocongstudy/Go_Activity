635bd03dfc80fa1a5ca977cfcd87641cfc1d21ef
guocong <guocong199708@163.com>
2021-07-30 02:23:42 -0700

    code net_coding3

---
+++ HelloWord/net_code/template/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+func main() {
+	//解析模版 字符串
+	text := "我的名字叫{{.}}"
+	tpl, err := template.New("test").Parse(text)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tpl.Execute(os.Stdout, "KK")
+	tpl.Execute(os.Stdout, 1)
+	tpl.Execute(os.Stdout, true)
+	tpl.Execute(os.Stdout, []string{"1", "2", "3"})
+
+}
